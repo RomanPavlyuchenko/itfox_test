c41bd2ce84aa0da8d53b47b359ea41c3b714d9e9
Roman Pavlyuchenko <romanpavlyuchenko@inbox.ru>
2023-03-24 16:23:15 +0300

    created news app

--- server/config/api_urls.py
+++ server/config/api_urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('auth/', include('apps.users.urls.auth')),
     path('users/', include('apps.users.urls.user')),
+    path('news/', include('apps.news.urls'))
 ]
--- server/config/settings/installed_apps.py
+++ server/config/settings/installed_apps.py
@@ -12,6 +12,7 @@ INSTALLED_APPS = [
 
 LOCAL_APPS = [
     'apps.users',
+    'apps.news',
 ]
 
 INSTALLED_APPS += LOCAL_APPS
